7bffebda13e9705e03f4cc802d79b73b913485c8
Francisco <jhersonhuacho@gmail.com>
2021-11-07 15:41:29 -0500

    Desafió de la clase 12 - Consigna 2 - agregar canal de chat

--- Desafios/Clase12-Desafío/server.js
+++ Desafios/Clase12-Desafío/server.js
@@ -12,12 +12,13 @@ const { Server } = require('socket.io');
 const io = new Server(httpServer);
 //
 const objProducts = new Contenedor("db");
+const objMensajes = new Contenedor("db-mensajes");
+const arrMensajes = [];
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/static', express.static(__dirname + "/public"));
-// app.use("/static", express.static(__dirname + "/public"));
 
 // set
 app.set("views", "./views");
@@ -33,46 +34,35 @@ app.engine(
 );
 
 // Configurar Websockets -Conexión Socket
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('usuario conectado');
 
-    // socket.on('saveProduct', async (data) => {
-    //     const productPerson = await objProducts.save(data);
-    //     const products = await objProducts.getAll();
-
-    //     io.sockets.emit('listProucts', products);
-    // });
-    socket.on('saveProduct', (data) => {
-        io.sockets.emit('listProucts', data);
+    const mensajes = await objMensajes.getAll();
+    io.sockets.emit('listarMensajes', mensajes);
+    
+    socket.on('saveProduct', async (data) => {
+        io.sockets.emit('listProducts', data);
     });
+    
+    socket.on('sendMensaje', async (data) => {
+        arrMensajes.push(data);
+        const productPerson = await objMensajes.save(data);
+        const mensajes = await objMensajes.getAll();
+        io.sockets.emit('listarMensajes', mensajes);
+    })
 });
 
 // Routes
 // app.use('/api/products', routerProducts);
 
-// app.get('/', (req, res) => {
-//     // res.sendFile(__dirname + '/public/index.html');
-//     res.render('main');
-// });
-
-// app.post('/productos', async (req, res) => {
-//     const product = req.body;
-//     const productPerson = await objProducts.save(product);
-//     // res.render('home', { layout: 'index', data: productPerson });
-//     res.redirect('/productos');
-// });
-
 app.get('/productos', async (req, res) => {
-    // console.log("hola");
     let products = await objProducts.getAll();
     let listExists = false;
-    // console.log("products", products);
+
     if (products.length !== 0) {
         listExists = true;
     }
-    // res.render('productos', { layout: 'index', data: products, listExists: listExists });
     res.render('main', { layout: 'index', data: products, listExists: listExists });
-    // res.render('productos');
 });
 
 // listen
