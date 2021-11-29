e74fe965241d4bf55cafee205627ef55aa750e3a
Francisco <jhersonhuacho@gmail.com>
2021-11-29 17:52:25 -0500

    desafío de la clase 16

---
+++ Desafios/Clase16-Desafío/public/index.js
@@ -0,0 +1,106 @@
+const socket = io();
+
+const listProductsHtml = ((data) => {
+  const tbody = document.querySelector('tbody');
+  const tr = document.createElement('tr');
+  const tdTitle = document.createElement('td');
+  const tdPrice = document.createElement('td');
+  const tdUrl = document.createElement('td');
+  tdTitle.innerText = data.title;
+  tdPrice.innerText = data.price;
+  tr.appendChild(tdTitle);
+  tr.appendChild(tdPrice);
+
+  const img = document.querySelector('img');
+  img.setAttribute('src', data.thumbnail);
+  tdUrl.appendChild(img);
+
+  tr.appendChild(tdUrl);
+  tbody.appendChild(tr);
+});
+
+const form = document.querySelector('.formulario');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const title = document.querySelector('#title');
+  const price = document.querySelector('#price');
+  const thumbnail = document.querySelector('#thumbnail');
+
+  const objProduct = {
+    title: title.value,
+    price: price.value,
+    thumbnail: thumbnail.value
+  }
+
+  fetch('/api/productos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(objProduct)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('data => ', data);
+      listProductsHtml(data);
+    });
+
+  title.value = '';
+  price.value = '';
+  thumbnail.value = '';
+})
+
+const formChat = document.querySelector('.formulario-chat');
+formChat.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const inputCorreo = document.querySelector('.input-correo');
+  const spanValidarInputCorreo = document.querySelector('.mensaje-validacion');
+
+  if (inputCorreo.value === '') {
+    spanValidarInputCorreo.classList.remove('none');
+    spanValidarInputCorreo.classList.add('block');
+    return;
+  } else {
+    spanValidarInputCorreo.classList.remove('block');
+    spanValidarInputCorreo.classList.add('none');
+  }
+
+  const inputMensaje = document.querySelector('.input-chat');
+  const fechaActual = new Date();
+  const fecha = `${fechaActual.toLocaleDateString()} ${fechaActual.getHours()}:${fechaActual.getMinutes()}:${fechaActual.getSeconds()}`;
+  const objMensaje = {
+    email: inputCorreo.value,
+    fecha: fecha,
+    mensaje: inputMensaje.value
+  }
+  inputMensaje.value = '';
+  socket.emit('sendMensaje', objMensaje);
+});
+
+socket.on('listarMensajes', (data) => {
+  const contentMensajes = document.querySelector('.content-mensajes');
+  contentMensajes.innerHTML = '';
+  console.log('data', data);
+  if (data.length !== 0) {
+    data.forEach(mensaje => {
+      console.log('mensaje', mensaje);
+      const p = document.createElement('p');
+      const spanEmail = document.createElement('span');
+      const spanFecha = document.createElement('span');
+      const spanMensaje = document.createElement('span');
+      spanEmail.innerText = `${mensaje.email} `;
+      spanEmail.style.color = 'blue';
+      p.appendChild(spanEmail);
+
+      spanFecha.innerText = `${mensaje.fecha} `;
+      spanFecha.style.color = 'red';
+      p.appendChild(spanFecha);
+
+      spanMensaje.innerText = `: ${mensaje.mensaje}`;
+      spanMensaje.style.color = 'green';
+      p.appendChild(spanMensaje);
+
+      contentMensajes.appendChild(p);
+    });
+  }
+});
---
+++ Desafios/Clase16-Desafío/server.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const knex = require('./db');
+const knexSqlite = require('./knexfile');
+// const Contenedor = require("./contenedor");
+const app = express();
+// const routerProducts = require('./routers/productos');
+// Server
+const http = require('http');
+const httpServer = http.createServer(app);
+const port = process.env.PORT || 8080;
+// Socket
+const { Server } = require('socket.io');
+const io = new Server(httpServer);
+//
+// const objProducts = new Contenedor("db");
+// const objMensajes = new Contenedor("db-mensajes");
+// const arrMensajes = [];
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// console.log('process.cwd()', process.cwd() + "/public");
+app.use('/static', express.static(__dirname + "/public"));
+
+// set
+app.set("views", "./views");
+app.set("view engine", "hbs");
+app.engine(
+    'hbs',
+    exphbs({
+        extname: 'hbs',
+        layoutsDir: __dirname + '/views/layouts',
+        defaultLayout: 'index',
+        partialsDir: __dirname + '/views/partials'
+    })
+);
+
+// Configurar Websockets -Conexión Socket
+io.on('connection', (socket) => {
+    console.log('usuario conectado');
+
+    knexSqlite
+        .from('chat')
+        .select('id', 'email', 'fecha', 'mensaje')
+        .then(response => {
+            // console.log('response =>', response);
+            io.sockets.emit('listarMensajes', response);
+        })
+
+    socket.on('sendMensaje', (data) => {
+        console.log('sendMensaje - data  => ', data)
+        knexSqlite('chat')
+            .insert(data)
+            .then(() => {
+                // console.log("Se registro correctamente el chat");
+                knexSqlite
+                    .from('chat')
+                    .select('id', 'email', 'fecha', 'mensaje')
+                    .then(reponse => {
+                        // console.log('sendMensaje  reponse', reponse);
+                        io.sockets.emit('listarMensajes', reponse);
+                    })
+            })
+            .catch((error) => {
+                console.log('Error al guardar => ', error);
+            });
+    })
+});
+
+app.get('/productos', (req, res) => {
+    knex
+        .from('product')
+        .select('id', 'title', 'price', 'thumbnail')
+        .then(products => {
+            // console.log('products', products);
+            let listExists = false;
+            if (products.length !== 0) {
+                listExists = true;
+            }
+            res.render('main', {
+                layout: 'index',
+                data: products,
+                listExists: listExists
+            });
+        })
+});
+
+// Routes API
+app.get('/api/productos', (req, res) => {
+    knex
+        .from('product')
+        .select('id', 'title', 'price', 'thumbnail')
+        .then(respone => {
+            // console.log('respone', respone);
+            res.json(respone)
+        })
+});
+
+app.post('/api/productos', (req, res) => {
+    console.log('req => ', req);
+    const data = {
+        title: req.body.title,
+        price: req.body.price,
+        thumbnail: req.body.thumbnail
+    }
+    knex('product')
+        .insert(data)
+        .then(() => {
+            console.log("Se registro correctamente el producto");
+            res.json(data)
+        })
+        .catch((error) => {
+            console.log('Error al guardar => ', error);
+        });
+});
+
+// listen
+httpServer.listen(port, () => {
+    console.log(`Example app listening at http//localhost:${port}/productos`);
+});
